c729edf70ca52f2e2883c1474b8b021ba0e3e6ab
Alexus <alexey.shapilov@gmail.com>
2015-09-01 17:58:32 +0300

    добавление задачи в БД

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    error_reporting(0);
+
+    define('DB_HOSTNAME', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'futuremes');
+
+    define('EMAIL', '');
+    define('SUBJECT', '');
